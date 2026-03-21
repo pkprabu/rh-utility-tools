e5b6dbcf9d8ce9c509bf1b9a11cc1831de4d1b5f
Prabu <pkprabu07@gmail.com>
2026-03-22 02:34:38 +0530

    Update config.js with new auditor settings
    Added new configuration options for email auditing.

--- email-auditor/js/config.js
+++ email-auditor/js/config.js
@@ -1,23 +1,56 @@
 export const config = {
-parameterName: "sc_cid",
+version: '1.1.0',
+parameterName: 'sc_cid',
 auditor: {
-    requiredImagePrefix: "https://explore.redhat.com/",
-    validFonts: ["RedHatText-Regular", "Red Hat Text", "Red Hat Display"],
-    validLinkColors: ["rgb(238, 0, 0)", "rgb(0, 102, 204)"],
-    ignoreList: [
-        "https://www.redhat.com/en/about/privacy-policy", "https://www.redhat.com/en/contact",
-        "https://www.redhat.com/en/preferences", "https://www.facebook.com/RedHatInc/",
-        "https://x.com/RedHat", "https://www.linkedin.com/company/red-hat",
-        "https://www.youtube.com/user/RedHatVideos", "https://explore.redhat.com/index.php/email/emailWebview"
-    ],
-    dummyKeywords: [
-        'lorem', 'ipsum', 'dolor', 'amet', 'consectetur', 'adipiscing', 'elit', 'nullam', 'aliquam',
-        'purus', 'iaculis', 'viverra', 'morbi', 'sollicitudin', 'convallis', 'curabitur', 'vestibulum'
-    ],
-    brandTextRules: {
-        redHatSpacing: {
-            regex: /\bRed(?!\u00A0)\s+Hat\b/gi
-        }
-    }
-}
+requiredImagePrefix: 'https://explore.redhat.com/',
+validFonts: [
+'RedHatText-Regular',
+'Red Hat Text',
+'Red Hat Display',
+],
+validLinkColors: [
+'#ee0000',
+'#0066cc',
+],
+ignoreList: [
+'https://www.redhat.com/en/about/privacy-policy',
+'https://www.redhat.com/en/contact',
+'https://www.redhat.com/en/preferences',
+'https://www.facebook.com/RedHatInc/',
+'https://x.com/RedHat',
+'https://www.linkedin.com/company/red-hat',
+'https://www.youtube.com/user/RedHatVideos',
+'https://explore.redhat.com/index.php/email/emailWebview',
+],
+dummyKeywords: [
+'lorem', 'ipsum', 'dolor', 'amet', 'consectetur',
+'adipiscing', 'elit', 'nullam', 'aliquam', 'purus',
+'iaculis', 'viverra', 'morbi', 'sollicitudin',
+'convallis', 'curabitur', 'vestibulum',
+],
+brandTextRules: {
+redHatSpacing: { regex: /\b(Red[ ]+Hat|RedHat)\b/gi },
+},
+minFontSize: 11,
+minLineHeight: 1.4,
+maxEmailWidth: 640,
+gmailClipThreshold: 102400,
+unsupportedCssProperties: [
+'display:flex', 'display:grid', 'display:inline-flex',
+'display:inline-grid', 'position:fixed', 'position:sticky',
+'animation', 'transition', 'transform',
+],
+placeholderImageDomains: [
+'localhost', '127.0.0.1', '0.0.0.0',
+'placehold.it', 'placeholder.com', 'via.placeholder.com',
+'picsum.photos', 'dummyimage.com', 'loremflickr.com',
+'placekitten.com', 'fakeimg.pl',
+],
+unsubscribeKeywords: [
+'unsubscribe', 'opt-out', 'opt out', 'optout',
+'manage preferences', 'email preferences',
+'remove me', 'manage subscriptions',
+],
+physicalAddressPattern: /\b\d+\s+\w[\w\s]{2,30}(?:st(?:reet)?|ave(?:nue)?|r(?:oa)?d|blvd|boulevard|dr(?:ive)?|ln|lane|way|court|ct|pl(?:ace)?|ter(?:race)?)\b|\b[A-Z]{1,2}\d{1,2}\s?\d[A-Z]{2}\b|\b\d{5}(?:-\d{4})?\b/i,
+},
 };
